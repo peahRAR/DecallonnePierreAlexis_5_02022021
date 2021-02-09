575f92fbc5f41bbe600f519c9db0ff1167a5f35e
Pierre-Alexis DECALLONNE <peah@iMac-de-Pierre-Alexis.local>
2021-02-09 23:42:46 +0100

    Erreur sur la page produit

--- main.js
+++ main.js
@@ -37,14 +37,14 @@ async function items() {
         // Div des cards (composant Bootstrap)
         itemCard.setAttribute("class", "card  mb-2");
         // Div Encart image
-        itemImg.setAttribute("class", "boxImg embed-responsive embed-responsive-16by9 "); 
+        itemImg.setAttribute("class", "boxImg embed-responsive embed-responsive-16by9 ");
         itemImg.setAttribute("height", "200px");
         // Balise image
         itemPhoto.setAttribute("src", item.imageUrl);
-        itemPhoto.setAttribute("alt" , "Photo d'une caméra");
-        itemPhoto.setAttribute("class" , "card-img-top embed-responsive-item");
+        itemPhoto.setAttribute("alt", "Photo d'une caméra");
+        itemPhoto.setAttribute("class", "card-img-top embed-responsive-item");
         // Div Body Card
-        itemDescBox.setAttribute("class", "card-body");   
+        itemDescBox.setAttribute("class", "card-body");
         // Balise Description
         itemDesc.setAttribute("class", "description");
         // Balise Link + Prix
@@ -54,7 +54,7 @@ async function items() {
         itemLink.setAttribute("href", "html/product.html?id=" + item._id); // Gére la redireciton en passant l'id produit dans l'url
         // Balise Prix
         itemPrice.setAttribute("class", "mb-0 font-weight-bold")
-        
+
         //Structure des blocs Html
         listItems.appendChild(itemPosition);
         itemPosition.appendChild(itemCard);
@@ -67,26 +67,70 @@ async function items() {
         itemPriceBox.appendChild(itemLink);
         itemPriceBox.appendChild(itemPrice);
 
-    
+
         // Affichage dans la page Html
         itemName.textContent = item.name;
         itemDesc.textContent = item.description;
-        itemPrice.textContent = item.price /100 + " EUR";
+        itemPrice.textContent = item.price / 100 + " EUR";
         itemLink.textContent = "more info";
 
     });
 }
 
-// Page produit
+//PAGE PRODUIT
 
 let idItems = "";
 async function infoItems() {
     idItems = location.search.substring(4);
     const infoItems = await findAllItems();
 
+    // Création des éléments html
     let infoItem = document.querySelector(".product");
+    let itemImg = document.createElement("div");
+    let itemDescBox = document.createElement("div");
+    let itemDesc = document.createElement("p");
+    let itemPhoto = document.createElement("img");
+    let itemName = document.createElement("h5");
+    let itemPriceBox = document.createElement("div")
+    let itemPrice = document.createElement("p");
+    let itemLink = document.createElement("a");
 
-    
+    // Ajout des attributs
+    infoItem.setAttribute("class", "card");
+    // Div des cards (composant Bootstrap)
+    itemCard.setAttribute("class", "card  mb-2");
+    // Div Encart image
+    itemImg.setAttribute("class", "boxImg embed-responsive embed-responsive-16by9 ");
+    itemImg.setAttribute("height", "200px");
+    // Balise image
+    itemPhoto.setAttribute("src", item.imageUrl);
+    itemPhoto.setAttribute("alt", "Photo d'une caméra");
+    itemPhoto.setAttribute("class", "card-img-top embed-responsive-item");
+    // Div Body Card
+    itemDescBox.setAttribute("class", "card-body");
+    // Balise Description
+    itemDesc.setAttribute("class", "description");
+    // Balise Link + Prix
+    itemPriceBox.setAttribute("class", "d-flex justify-content-between align-items-center")
+    // Balise Link
+    itemLink.setAttribute("class", "btn badge badge-dark text-uppercase p-2")
+    itemLink.setAttribute("href", "html/product.html?id=" + item._id); // Gére la redireciton en passant l'id produit dans l'url
+    // Balise Prix
+    itemPrice.setAttribute("class", "mb-0 font-weight-bold")
+
+    //Structure des blocs Html
+    itemPosition.appendChild(itemCard);
+    itemCard.appendChild(itemImg);
+    itemImg.appendChild(itemPhoto);
+    itemCard.appendChild(itemDescBox);
+    itemDescBox.appendChild(itemName);
+    itemDescBox.appendChild(itemDesc);
+    itemDescBox.appendChild(itemPriceBox);
+    itemPriceBox.appendChild(itemLink);
+    itemPriceBox.appendChild(itemPrice);
+
+    // Remplissage des balise
+    itemName.textContent = infoItems.name
 }
 
 console.log(items()); // Verification que le tableau d'objet issu de l'API est bien return
